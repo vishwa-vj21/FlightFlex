90adbf7b3f09d14d66cb02055b3f4f8872fd4200
vishwa.vj <iamvishwajith.s@gmail.com>
2025-04-16 17:26:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/flightflex/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.flightflex
+
+import android.os.Bundle
+
+
+//package com.example.yourapp
+
+//import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) // 👈 Tells Android to use your XML layout
+    }
+}
+
+
+
+
+
